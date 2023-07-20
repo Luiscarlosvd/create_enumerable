471dd210baef307dfe2acbd404fc65bf06ed0c71
Luiscarlosvd <lc26cr7@gmail.com>
2023-07-20 12:35:58 -0400

    Fix enumerable path and add tests

--- list.rb
+++ list.rb
@@ -1,4 +1,4 @@
-require_relative MyEnumerable
+require_relative 'enumerable'
 
 class MyList
   include MyEnumerable
@@ -10,3 +10,23 @@ class MyList
     @list.each(&block)
   end
 end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+# <MyList: @list=[1, 2, 3, 4]>
+
+# Test #all?
+puts(list.all? { |e| e < 5 })
+# true
+puts(list.all? { |e| e > 5 })
+# false
+
+# Test #any?
+puts(list.any? { |e| e == 2 })
+# true
+puts(list.any? { |e| e == 5 })
+# false
+
+# Test #filter
+p(list.filter(&:even?))
+# [2, 4]
